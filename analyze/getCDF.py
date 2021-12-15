6b6d344b7f22cd9940867fefb6083f314225e509
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2021-12-16 00:21:09 +0800

    mod getCDF.py reduce .cdf size

--- analyze/getCDF.py
+++ analyze/getCDF.py
@@ -19,14 +19,14 @@ def getCDF(file):
     with open(rawPath, 'r', encoding='utf-8') as rawF:
         with open(cdfPath, 'w', encoding='utf-8') as cdfF:
             csvReader = csv.reader(rawF)
-            timer = 0
+            # timer = 0
             for row in csvReader:
                 if len(row) == 3 and row[1][-1].isdigit():
                     ip_set.add(row[1].strip())
-                cdfF.write('{} {}\n'.format(timer, round(len(ip_set)/max, 2)))
-                timer += 1
+                # cdfF.write('{} {}\n'.format(timer, round(len(ip_set)/max, 2)))
+                cdfF.write('{}\n'.format(round(len(ip_set)/max, 2)))
+                # timer += 1
 
 
 for file in os.listdir():
-    if file.endswith(".csv") and not file.endswith("top.csv"):
-        getCDF(file)
+    getCDF(file)
