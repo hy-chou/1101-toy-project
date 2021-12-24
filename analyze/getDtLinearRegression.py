d7dc5e1b8e3515b600bdc3479388ceeb87f7c1b1
Hsuan-Yu Chou <55382796+hy-chou@users.noreply.github.com>
2021-12-24 11:24:08 +0800

    style getDtLinearRegression.py

--- analyze/getDtLinearRegression.py
+++ analyze/getDtLinearRegression.py
@@ -19,11 +19,17 @@ with open(argv[1], 'r', encoding='utf-8') as f:
 
 m_min = round(polyfit(x, y_min, 1)[0], 3)
 b_min = round(polyfit(x, y_min, 1)[1], 3)
+if b_min >= 0:
+    b_min = '+{}'.format(b_min)
 m_avg = round(polyfit(x, y_avg, 1)[0], 3)
 b_avg = round(polyfit(x, y_avg, 1)[1], 3)
+if b_avg >= 0:
+    b_avg = '+{}'.format(b_avg)
 m_max = round(polyfit(x, y_max, 1)[0], 3)
 b_max = round(polyfit(x, y_max, 1)[1], 3)
+if b_max >= 0:
+    b_max = '+{}'.format(b_max)
 
-print('min: {}x + {}'.format(m_min, b_min))
-print('avg: {}x + {}'.format(m_avg, b_avg))
-print('max: {}x + {}'.format(m_max, b_max))
+print('min: {}*x{}'.format(m_min, b_min))
+print('avg: {}*x{}'.format(m_avg, b_avg))
+print('max: {}*x{}'.format(m_max, b_max))
