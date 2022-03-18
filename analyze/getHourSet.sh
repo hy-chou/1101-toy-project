92d7ed832a1e56db8f6ace62d14bb64fbe5dfafb
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-03-19 00:33:25 +0800

    mod new dd option

--- analyze/getHourSet.sh
+++ analyze/getHourSet.sh
@@ -8,10 +8,12 @@ dir="$1"
 
 cd ${dir}
 
+dd="*"
+
 for hh in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 do
-  echo ${hh}
-  python3 ~/1101-toy-project/analyze/getIPSet.py 12T${hh} > ../2022-03-12T${hh}.iplist
+  echo ${dd}T${hh}
+  python3 ~/1101-toy-project/analyze/getIPSet.py ${dd}T${hh} > ../2022-03-${dd}T${hh}.iplist
 done
 
 sleep 1
