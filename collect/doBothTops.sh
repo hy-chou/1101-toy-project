0dc7d197d598d06c15b8568146f5e60a4611cc88
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-03-19 00:38:50 +0800

    fix use absolute path

--- collect/doBothTops.sh
+++ collect/doBothTops.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 t0=$(date --iso-8601="seconds")
-top -u samuel | head -n 20 > ${t0}.top
-iftop -ts 60 > ${t0}.iftop &
+top -u samuel | head -n 20 > /home/samuel/${t0}.top
+iftop -ts 60 > /home/samuel/${t0}.iftop &
 
 sleep 1
 echo "All done."
-
