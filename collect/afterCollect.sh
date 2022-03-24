47fd9fd53825e5a1f78069c9534c380e412a250b
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-03-24 23:58:49 +0800

    mod aftercollect.sh too many files

--- collect/afterCollect.sh
+++ collect/afterCollect.sh
@@ -1,21 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-if [ "$1" == "" ]; then
-    echo "usage: bash afterCollect.sh dir"
-    exit 1
-fi
-dir="$1"
+# mkdir raws vcnts csvs
+# mv 2022*raw.json raws/.
+# mv 2022*000.csv vcnts/.
+# mv [0-2][0-9]T*.csv csvs/.
 
-cd ${dir}
-mkdir raws vcnts csvs
-mv *-*raw.json raws/.
-mv *-*000.csv vcnts/.
-mv 17T*.csv csvs/.
-mv 18T*.csv csvs/.
-
-cd ..
-nohup xz -9 ${dir}/nohup.out >/dev/null 2>&1 &
-nohup xz -9 ${dir}/error.err >/dev/null 2>&1 &
-
-sleep 1
-echo "All done."
+# nohup xz -9 ${dir}/nohup.out >/dev/null 2>&1 &
+# nohup xz -9 ${dir}/error.err >/dev/null 2>&1 &
