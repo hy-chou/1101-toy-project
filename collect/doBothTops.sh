932930b1653b742b4e5c93d446869ec061b9e74e
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-03-24 23:59:37 +0800

    mod doBothTops.sh use nohup

--- collect/doBothTops.sh
+++ collect/doBothTops.sh
@@ -2,7 +2,7 @@
 
 t0=$(date --iso-8601="seconds")
 top -u samuel | head -n 20 > /home/samuel/${t0}.top
-iftop -ts 60 > /home/samuel/${t0}.iftop &
+nohup iftop -ts 60 > /home/samuel/${t0}.iftop 2>&1 &
 
 sleep 1
 echo "All done."
