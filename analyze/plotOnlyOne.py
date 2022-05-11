d478225a641381f7d2ae16f419019a1fefce9336
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-05-12 01:10:39 +0800

    fix plotOO

--- analyze/plotOnlyOne.py
+++ analyze/plotOnlyOne.py
@@ -13,11 +13,11 @@ for DDThh in DDThhs:
     cdf.update({DDThh: []})
     keyword = f'2022-05-{DDThh}vcnt1001_'
 
-    for file in listdir('./tops'):
+    for file in listdir('./vcnts'):
         if keyword != '' and keyword not in file:
             continue
         print(file)
-        with open('./tops/' + file) as f:
+        with open('./vcnts/' + file) as f:
             lines = f.readlines()
         ulogins = lines[-2][:-1].split('\t')
 
@@ -26,7 +26,7 @@ for DDThh in DDThhs:
 
     for topline in range(1, 201):
         for ulogin in ulogins[:topline]:
-            file = f'2022-03-{DDThh}{ulogin}.tsv'
+            file = f'2022-05-{DDThh}{ulogin}.tsv'
             with open('./tsvs/' + file, 'r') as f:
                 line = f.readline()
                 l = line.split('\t')
@@ -45,7 +45,7 @@ fig, ax = plt.subplots(figsize=(12.8, 4.8))
 
 for DDThh in cdf:
     noise = random()
-    x = [i + 0.5 * noise for i in range(1, 201)]
+    x = [i + 0.5 * noise for i in range(1001, 1201)]
     y = [i + 0.2 * noise for i in cdf[DDThh]]
     ax.step(x, y)
 
@@ -55,5 +55,5 @@ ax.set_ylabel('number of IP addresses collected')
 cwd = getcwd()
 cwd = cwd[cwd.rfind('/') + 1:]
 ax.set_title(f'{cwd}')
-plt.savefig(f'oo_1.png', bbox_inches='tight')
+plt.savefig(f'oo_1001.png', bbox_inches='tight')
 plt.close(fig)
