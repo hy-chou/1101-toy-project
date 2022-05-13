68f8206df1aa4048a77cf509a2de313b7d40c96f
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-05-13 16:46:00 +0800

    mod good for try40 to try0060

--- analyze/plotOnlyOne.py
+++ analyze/plotOnlyOne.py
@@ -1,59 +1,73 @@
-from os import getcwd, listdir
+from cProfile import label
+from os import getcwd, listdir, path
 from random import random
 
 import matplotlib.pyplot as plt
 
-DDThhs = set()
+ts2Hs = set()
 for file in listdir('./vcnts'):
-    DDThhs.add(file[8:13])
-DDThhs = sorted(list(DDThhs))
+    # for file in listdir('./tops'):
+    ts2Hs.add(file[:13])
+ts2Hs = sorted(list(ts2Hs))
+
+n = 200
 
 cdf = dict()
-for DDThh in DDThhs:
-    cdf.update({DDThh: []})
-    keyword = f'2022-05-{DDThh}vcnt1001_'
+for ts2H in ts2Hs:
+    cdf.update({ts2H: []})
+    keyword = f'{ts2H}vcnt1_'
+    # keyword = f'{ts2H}vcnt0001_'
+    # keyword = f'{ts2H}top'
 
     for file in listdir('./vcnts'):
-        if keyword != '' and keyword not in file:
+        # for file in listdir('./tops'):
+        if keyword not in file:
             continue
         print(file)
         with open('./vcnts/' + file) as f:
+            # with open('./tops/' + file) as f:
             lines = f.readlines()
         ulogins = lines[-2][:-1].split('\t')
+        # ulogins = lines[-2][:-1].split(', ')
 
     ipcount = dict()
     errcount = dict()
 
-    for topline in range(1, 201):
-        for ulogin in ulogins[:topline]:
-            file = f'2022-05-{DDThh}{ulogin}.tsv'
-            with open('./tsvs/' + file, 'r') as f:
-                line = f.readline()
-                l = line.split('\t')
+    for ulogin in ulogins[:n]:
+        file = f'{ts2H}{ulogin}.tsv'
+        # file = f'{ts2H[-5:]}{ulogin}.tsv'
+        # file = f'{ts2H[-5:]}{ulogin}.csv'
+        # if not path.isfile('./csvs/' + file):
+        #     continue
+        with open('./tsvs/' + file, 'r') as f:
+            # with open('./csvs/' + file, 'r') as f:
+            line = f.readline()
+            l = line.split('\t')
+            # l = line.split('","')
 
-                # count IP
-                if l[1][0].isdigit():
-                    if l[1] in ipcount:
-                        ipcount[l[1]] += 1
-                    else:
-                        ipcount.update({l[1]: 1})
-        cdf[DDThh].append(len(ipcount))
+            # count IP
+            if l[1][0].isdigit():
+                if l[1] in ipcount:
+                    ipcount[l[1]] += 1
+                else:
+                    ipcount.update({l[1]: 1})
+        cdf[ts2H].append(len(ipcount))
 
 # plot
 
-fig, ax = plt.subplots(figsize=(12.8, 4.8))
+fig, ax = plt.subplots(figsize=(12.8 * (n / 200), 4.8))
 
-for DDThh in cdf:
+for ts2H in cdf:
     noise = random()
-    x = [i + 0.5 * noise for i in range(1001, 1201)]
-    y = [i + 0.2 * noise for i in cdf[DDThh]]
-    ax.step(x, y)
-
+    x = [i + 0.5 * noise for i in range(1, len(cdf[ts2H]) + 1)]
+    y = [i + 0.2 * noise for i in cdf[ts2H]]
+    ax.step(x, y, label=ts2H[-5:])
 
 ax.set_xlabel('number of top channels included')
 ax.set_ylabel('number of IP addresses collected')
+ax.legend(ncol=1, loc='lower right')
 cwd = getcwd()
 cwd = cwd[cwd.rfind('/') + 1:]
 ax.set_title(f'{cwd}')
-plt.savefig(f'oo_1001.png', bbox_inches='tight')
+plt.savefig(f'{cwd}.oo1_{n}.png', bbox_inches='tight')
 plt.close(fig)
