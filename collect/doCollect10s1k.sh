abd14ae0b741cd20d91b80c5a48330d5c6337fef
Hsuan-Yu Chou <55382796+hy-chou@users.noreply.github.com>
2022-05-18 23:52:37 +0800

    new collect freqencies

---
+++ collect/doCollect10s1k.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "$4" == "" ]; then
+    echo "usage: bash doCollect10s1k.sh dir dd hh he"
+    echo ""
+    echo -e "dir\ttarget directory"
+    echo -e "dd\tlocal date"
+    echo -e "[hh, he]\n\tlocal hour"
+    exit 0
+fi
+
+dir="$1"
+dd="$2"
+hh="$3"
+he="$4"
+cmd="nohup node ../scheduler1k.js"
+
+test ! -d ${dir} && mkdir ${dir}
+cd ${dir}
+
+while [ ! ${hh} == $((${he}+1)) ]
+do
+    ${cmd} "*/10 00-19 ${hh} ${dd} * *" "0 35 ${hh} ${dd} * *" >/dev/null 2>&1 &
+    ${cmd} "*/10 20-39 ${hh} ${dd} * *" "0 55 ${hh} ${dd} * *" >/dev/null 2>&1 &
+    if [ ${hh} != 23 ]; then
+        ${cmd} "*/10 40-59/2 ${hh} ${dd} * *" "0 15 $((${hh}+1)) ${dd} * *" >/dev/null 2>&1 &
+        hh="$((${hh}+1))"
+    else
+        ${cmd} "*/10 40-59/2 23 ${dd} * *" "0 15 0 $((${dd}+1)) * *" >/dev/null 2>&1 &
+        sleep 1
+        echo "All done."
+        exit 0
+    fi
+done
+
+sleep 1
+echo "All done."
