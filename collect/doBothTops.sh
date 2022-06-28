eecb036a738eb747bc0de975fe344129e227935d
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-06-28 17:39:46 +0800

    rm old version scripts

--- collect/doBothTops.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-t0=$(date --iso-8601="seconds")
-top -u samuel | head -n 20 > /home/samuel/${t0}.top
-nohup iftop -ts 60 > /home/samuel/${t0}.iftop 2>&1 &
-
-sleep 1
-echo "All done."
--- collect/old-version/scheduleTop.js
+++
@@ -1,6 +0,0 @@
-const cron = require("node-cron");
-const process = require("process");
-const { getMidIP } = require("./getMidIP.js");
-
-cron.schedule(process.argv[2], () => getMidIP(1, 1000));
-cron.schedule(process.argv[3], () => process.exit());
