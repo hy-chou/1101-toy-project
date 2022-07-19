cf7bcfcbffc428f2d68a7280c40f419d4c0389c9
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-07-19 22:11:49 +0800

    mod plot hist real frequency

--- analyze/plot_real_freq.py
+++ analyze/plot_real_freq.py
@@ -1,33 +1,39 @@
-from os import listdir, getcwd
 from datetime import datetime
+from os import getcwd, listdir
+from random import sample
+from time import process_time
+
 import matplotlib.pyplot as plt
 
 
-def get_real_freq():
-    dreqs = []
-    for file in listdir('./tsvs'):
-        dreq = []
+def get_real_freq(grouped_by_file=False):
+    rfs = []
+
+    for file in sample(listdir('./tsvs'), 1000):
+        rf = []
+
         with open(f'./tsvs/{file}') as f:
-            lines = f.readlines()
+            lines = sorted(f.readlines())
 
         t_prev = datetime.fromisoformat(lines[0][:23])
         for line in lines[1:]:
             t_curr = datetime.fromisoformat(line[:23])
-            dreq.append((t_curr - t_prev).total_seconds())
+            dt = (t_curr - t_prev).total_seconds()
+            rf.append(dt if (dt < 3) else -1)
             t_prev = t_curr
-        dreqs.append(dreq)
-        if len(dreqs) == 500:
-            break
-    return dreqs
+        if grouped_by_file:
+            rfs.append(rf)
+        else:
+            rfs += rf
+    return rfs
+
 
 def plot_real_freq():
-    dreqs = get_real_freq()
-
     fig, ax = plt.subplots()
     fig.suptitle('real frequency')
 
-    for dreq in dreqs:
-        ax.plot(dreq, '.')
+    for rf in get_real_freq(grouped_by_file=True):
+        ax.plot(rf, '.')
 
     cwd = getcwd()
     cwd = cwd[cwd.rfind('/') + 1:]
@@ -38,5 +44,25 @@ def plot_real_freq():
     plt.savefig(f'./plots/real_freq.png', bbox_inches='tight')
     plt.close(fig)
 
+
+def hist_real_freq():
+    fig, ax = plt.subplots()
+    fig.suptitle('real frequency')
+
+    ax.hist(get_real_freq(), bins=100, histtype='stepfilled')
+
+    cwd = getcwd()
+    cwd = cwd[cwd.rfind('/') + 1:]
+    ax.set_title(cwd)
+    ax.set_xlabel('1 / frequency (sec)')
+    ax.set_ylabel('count')
+
+    plt.savefig(f'./plots/real_freq.png', bbox_inches='tight')
+    plt.close(fig)
+
+
 if __name__ == '__main__':
-    plot_real_freq()
+    pt = process_time()
+    # plot_real_freq()
+    hist_real_freq()
+    print(process_time() - pt)
