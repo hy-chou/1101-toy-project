265625d8eeb09aa708361579f44152e8f1858c1e
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-07-21 00:17:30 +0800

    add bursty scripts

---
+++ collect/getActiveEdgesBursty.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+const path = require("path");
+const cron = require("node-cron");
+const process = require("process");
+const { getEdgeAddr } = require("./getEdgeAddrLocal.js");
+
+
+const handleError = async (err, location) => {
+  const ts = new Date().toISOString();
+  const lines = `${ts}\t${location}\t${err}\n`;
+
+  fs.appendFileSync(path.join(process.cwd(), "error.err"), lines);
+};
+
+const waitAMinute = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+const calcDts = (ts1) => (new Date() - ts1) / 1000;
+
+const readUserLogins = async (c) => {
+  const ulgPath = path.join(process.cwd(), `ulg${c}.tsv`);
+  const ts1 = new Date();
+
+  while (!fs.existsSync(ulgPath)) {
+    await waitAMinute();
+    handleError(`g.${c} waited ${calcDts(ts1)} sec`, "@ readUserLogins()");
+
+    if (calcDts(ts1) > 60) {
+      handleError(`g.${c} stopped waiting`, "@ readUserLogins()");
+      return [];
+    }
+  }
+
+  let lines = fs.readFileSync(ulgPath, "utf8").split("\n");
+
+  return lines[lines.length - 2].split("\t");
+};
+
+const writeEdge = async (channel, ts1, addr) => {
+  const dts = (new Date() - ts1)/1000;
+  const ts2H = ts1.toISOString().slice(0, 13);
+  const lines = ts1.toISOString() + "\t" + addr + "\t" + dts + "\n";
+  const tsvPath = path.join(process.cwd(), `${ts2H}${channel}.tsv`);
+
+  try {
+    fs.appendFileSync(tsvPath, lines);
+  } catch (err) {
+    handleError(err, "@ writeEdge()");
+  }
+};
+
+const getEdgeFromUserLogin = async (ulogin) => {
+  const ts1 = new Date();
+
+  await getEdgeAddr(ulogin)
+  .then((addr) => writeEdge(ulogin, ts1, addr))
+  .catch((error) => writeEdge(ulogin, ts1, error.message));
+};
+
+const collectEdgesOfGroup = async (c, burstMode=true) => {
+  const ulogins = (await readUserLogins(c));
+
+  if (burstMode) return Promise.all(ulogins.map(item => getEdgeFromUserLogin(item)))
+
+  return ulogins.reduce(
+    (preVal, _, i) => preVal.then(() => getEdgeFromUserLogin(ulogins[i])),
+    Promise.resolve()
+  )
+};
+
+const main = async (c1=1, cn=100, groupSize=100) => {
+  let cs = [c1];
+
+  while (cs[cs.length - 1] + groupSize - 1 < cn) {
+    cs.push(cs[cs.length - 1] + groupSize);
+  }
+
+  return Promise.all(cs.map(c => collectEdgesOfGroup(c)));
+};
+
+if (require.main === module) {
+  const pargv = process.argv;
+  const stopProcess = () => process.kill(process.pid, 'SIGTERM');
+
+  switch (pargv.length) {
+    case 2:
+      main()
+      .then(stopProcess);
+      break;
+    case 4:
+      main(Number(pargv[2]), Number(pargv[3]))
+      .then(stopProcess);
+      break;
+    case 6:
+      cron.schedule(pargv[4], () => main(Number(pargv[2]), Number(pargv[3])));
+      cron.schedule(pargv[5], stopProcess);
+      break;
+    default:
+      console.log("Error:  wrong argv")
+      stopProcess();
+  }
+}
