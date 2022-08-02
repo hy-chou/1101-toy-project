de6e97d5341ee0cdb8f849b826451e391c4c9be6
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-08-02 21:37:00 +0800

    mod report style

--- collect/report.sh
+++ collect/report.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 du -hd 0
-
-mkdir tsvs txts ulgs
-mv 2* tsvs/
-mv *.txt txts/
-mv u*v ulgs/
-
+echo
+test ! -d tsvs && mkdir tsvs || echo "tsvs/ exists"
+test ! -d txts && mkdir txts || echo "txts/ exists"
+test ! -d ulgs && mkdir ulgs || echo "ulgs/ exists"
+test -f 2*    && mv 2*    tsvs/ || echo "2* DNE"
+test -f *.txt && mv *.txt txts/ || echo "*.txt DNE"
+test -f u*v   && mv u*v   ulgs/ || echo "u*v DNE"
+echo
 python3 ../../analyze/report_all.py
-
+echo
+ls ulgs/ | wc -l
+echo
 cat ulgs/* | wc -w
-
+echo
 cat tsvs/* | wc -l
-
+echo
 cat tsvs/* | cut -f 2 | sort | uniq -c
-
+echo
 sleep 1
 echo "all done."
