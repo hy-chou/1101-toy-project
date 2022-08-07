890dd3895681f4839557baf96d3ccaa9de43bfed
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-08-07 11:44:17 +0800

    mod report mkdir and test

--- collect/report.sh
+++ collect/report.sh
@@ -2,12 +2,12 @@
 
 du -hd 0
 echo
-test ! -d tsvs && mkdir tsvs || echo "tsvs/ exists"
-test ! -d txts && mkdir txts || echo "txts/ exists"
-test ! -d ulgs && mkdir ulgs || echo "ulgs/ exists"
-test -f 2*    && mv 2*    tsvs/ || echo "2* DNE"
-test -f *.txt && mv *.txt txts/ || echo "*.txt DNE"
-test -f u*v   && mv u*v   ulgs/ || echo "u*v DNE"
+mkdir tsvs
+mkdir txts
+mkdir ulgs
+mv 2*    tsvs/
+mv *.txt txts/
+mv u*v   ulgs/
 echo
 python3 ../../analyze/report_all.py
 echo
