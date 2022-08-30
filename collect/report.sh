70a517cd4a2f8d4037c03d8875ea616162b65cc0
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-08-30 18:34:58 +0800

    style report

--- collect/report.sh
+++ collect/report.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-echo "report.sh started."
-touch report.txt
+if [ -f report.txt ] ; then
+    cat report.txt
+		exit 0
+fi
 
-du -hd 0 >> report.txt
-echo -e "\n-=-\n" >> report.txt
-python3 ../../analyze/report/report.py  >> report.txt
-echo -e "\n-=-\n" >> report.txt
-echo -e "             \tULG\tULGS\tTSVS\tERRS" >> report.txt
+echo "report.sh running..."
+
+LINES="# REPORT
+$(date -Iseconds)
+
+## File Space Usage
+
+$(du -hd 0)
+
+## Network & Machine
+
+$(python3 ../../analyze/report/report.py)
+
+## Reply Rates
+$(echo -e "             \tULG\tULGS\tTSVS\tERRS")"
+
 for h in $(ls ulgs)
 do
 	ULG=$(ls ulgs/$h | wc -l)
 	ULGS=$(cat ulgs/$h/* | wc -w)
 	TSVS=$(find tsvs/$h -name "*.tsv" | xargs cat | wc -l)
 	ERRS=$(find tsvs/$h -name "*.tsv" | xargs cat | cut -f 2 | grep -v "[0-9][.][0-9]\+[.][0-9]\+[.][0-9]" | wc -l)
-	echo -e "$h\t${ULG}\t${ULGS}\t${TSVS}\t${ERRS}" >> report.txt
+
+	LINES="${LINES}
+$(echo -e "$h\t${ULG}\t${ULGS}\t${TSVS}\t${ERRS}")"
 done
 
-echo "report.sh done."
-cat report.txt
+echo "${LINES}" >> report.txt
+echo "${LINES}"
