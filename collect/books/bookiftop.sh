e59104c478df4b3920b5fa606f01eda8108455e8
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-09-02 18:59:14 +0800

    fix books

--- collect/books/bookiftop.sh
+++ collect/books/bookiftop.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 if [ $# != 1 ] ; then
-    echo    "NAME"
-    echo -e "\tbookiftop.sh - schedule iftop with cron"
-    echo
-    echo    "DIRECTIONS"
-    echo -e "\tWrite the following lines to /etc/cron.d/bookiftop"
-    echo
-    echo -e "\t\tSHELL=/bin/sh"
-    echo -e "\t\tPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-    echo -e "\t\tDIR_B=$(cd ../books && pwd)"
-    echo -e "\t\tDIR_C=$(cd .. && pwd)"
-    echo -e "\t\t*  *  *  *  * root bash \${DIR_B}/bookiftop.sh \${DIR_C}/try2037_30k 1 30000"
-    echo
-    echo    "ARGUMENTS"
-    echo -e "\tDIR\tabsolute path to the directory to store the data"
-    echo -e "\t   \te.g. $HOME/try2035_30k"
+    echo -en "
+NAME
+  \tbookiftop.sh - schedule iftop with cron
 
+ARGUMENTS
+  \tDIR\tabsolute path to the directory to store the data
+  \t   \te.g. $HOME/try2038_30k
+
+DIRECTIONS
+  \tWrite the following lines to /etc/cron.d/bookiftop
+
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+DIR_C=$(cd .. && pwd)
+
+ *  *  *  *  * root bash \${DIR_C}/books/bookiftop.sh \${DIR_C}/try2038_30k
+"
     exit 0
 fi
 
--- collect/books/booktop.sh
+++ collect/books/booktop.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 if [ $# != 1 ] ; then
-    FILENAME="booktop.sh"
+    echo -en "
+NAME
+  \tbooktop.sh - schedule top with cron
 
-    echo    "NAME"
-    echo -e "\t${FILENAME} - schedule top with cron"
-    echo
-    echo    "DIRECTIONS"
-    echo -e "\tWrite the following lines to /etc/cron.d/booktop"
-    echo
-    echo -e "\t\tSHELL=/bin/sh"
-    echo -e "\t\tPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-    echo -e "\t\tDIR_B=$(cd ../books && pwd)"
-    echo -e "\t\tDIR_C=$(cd .. && pwd)"
-    echo -e "\t\t*  *  *  *  * $USER bash \${DIR_B}/booktop.sh \${DIR_C}/try2037_30k 1 30000"
-    echo
-    echo    "ARGUMENTS"
-    echo -e "\tDIR\tabsolute path to the directory to store the data"
-    echo -e "\t   \te.g. $HOME/try2035_30k"
+ARGUMENTS
+  \tDIR\tabsolute path to the directory to store the data
+  \t   \te.g. $HOME/try2038_30k
+
+DIRECTIONS
+  \tWrite the following lines to /etc/cron.d/booktop
+
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+DIR_C=$(cd .. && pwd)
+
+ *  *  *  *  * $USER bash \${DIR_C}/books/booktop.sh \${DIR_C}/try2038_30k
+"
 
     exit 0
 fi
