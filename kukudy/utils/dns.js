7035558f0439ccae727164dbb4c57c556045a562
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-10-07 14:48:56 +0800

    mod dns, do not log cache hit/miss

--- kukudy/utils/dns.js
+++ kukudy/utils/dns.js
@@ -1,7 +1,7 @@
 const dns = require('node:dns');
 const { readdir, readFile } = require('node:fs/promises');
 
-const { writeData, getTS } = require('./utils');
+const { getTS } = require('./utils');
 
 const loadDNSCache = async () => {
   const ts2M = getTS().slice(0, 7);
@@ -16,20 +16,13 @@ const loadDNSCache = async () => {
 };
 
 const lookup = async (kache, hostname) => {
-  const ts = getTS();
-  const ts2M = ts.slice(0, 7);
-
   if (kache.has(hostname)) {
-    writeData(`./logs/cache/${ts2M}.tsv`, `${ts}\t${hostname}\tHIT\n`);
-
     return kache.get(hostname);
   }
 
   const ipv4 = (await dns.promises.lookup(hostname)).address;
 
   kache.set(hostname, ipv4);
-  writeData(`../cache/${ts2M}/${hostname}`, `${ts}\t${ipv4}\n`);
-
   return ipv4;
 };
 
