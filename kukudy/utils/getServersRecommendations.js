abcf0651a6cde5d4d3ec4f0ad0ab8c7c8a7a170e
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-10-16 16:40:33 +0800

    mod gSvrRcmd, specify country in argv

--- kukudy/utils/getServersRecommendations.js
+++ kukudy/utils/getServersRecommendations.js
@@ -1,24 +1,84 @@
+/* eslint-disable no-console */
 const axios = require('axios');
-const countryID = require('./nordVPNCountryList').countryList.TW;
 
-const url = 'https://nordvpn.com/wp-admin/admin-ajax.php';
-const config = {
-  params: {
-    action: 'servers_recommendations',
-    filters: {
-      country_id: countryID,
-      servers_technologies: [3],
-    },
-  },
+const countryCodes = {
+  AL: 2,
+  AR: 10,
+  AU: 13,
+  AT: 14,
+  BE: 21,
+  BA: 27,
+  BR: 30,
+  BG: 33,
+  CA: 38,
+  CL: 43,
+  CR: 52,
+  HR: 54,
+  CY: 56,
+  CZ: 57,
+  DK: 58,
+  EE: 68,
+  FI: 73,
+  FR: 74,
+  GE: 80,
+  DE: 81,
+  GR: 84,
+  HK: 97,
+  HU: 98,
+  IS: 99,
+  ID: 101,
+  IE: 104,
+  IL: 105,
+  IT: 106,
+  JP: 108,
+  KR: 114,
+  LV: 119,
+  LT: 125,
+  LU: 126,
+  MK: 128,
+  MY: 131,
+  MX: 140,
+  MD: 142,
+  NL: 153,
+  NZ: 156,
+  NO: 163,
+  PL: 174,
+  PT: 175,
+  RO: 179,
+  RS: 192,
+  SG: 195,
+  SK: 196,
+  SI: 197,
+  ZA: 200,
+  ES: 202,
+  SE: 208,
+  CH: 209,
+  TW: 211,
+  TH: 214,
+  TR: 220,
+  UA: 225,
+  UK: 227,
+  US: 228,
+  VN: 234,
 };
 
-const getServersRecommendations = async () => axios.get(url, config)
-  .then((res) => res.data.map(
-    (server) => server.hostname.slice(0, server.hostname.indexOf('.')),
-  ));
-
 if (require.main === module) {
-  getServersRecommendations()
-    // eslint-disable-next-line no-console
-    .then((serverID) => console.log(serverID.join('\t')));
+  const country = process.argv[2];
+  const countryNum = countryCodes[country];
+
+  if (countryNum === undefined) {
+    console.error(`bad country code:  ${country}`);
+    process.exit(1);
+  }
+  axios.get('https://nordvpn.com/wp-admin/admin-ajax.php', {
+    params: {
+      action: 'servers_recommendations',
+      filters: {
+        country_id: countryCodes[process.argv[2]],
+        servers_technologies: [3],
+      },
+    },
+  })
+    .then((res) => res.data[0].hostname)
+    .then((hostname) => console.log(hostname.slice(0, hostname.indexOf('.'))));
 }
