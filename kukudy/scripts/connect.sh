53d50d3ff8efca4d1fd71ab86328fd36a314367f
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-10-16 15:55:46 +0800

    mod connect, pure connect w/o uE, uS

--- kukudy/scripts/connect.sh
+++ kukudy/scripts/connect.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-DIR_C="/etc/openvpn/confs"
-
 SERVER_ID="$1"
-TARGET_DIR="$2"
-NUM_CHANNEL="$3"
 
+DIR_N="/etc/openvpn/nordvpn"
+CONF="${SERVER_ID}.nordvpn.com.udp.ovpn"
+TS="$(date -I)"
 
-mkdir -p ${TARGET_DIR}/logs
-cd ${TARGET_DIR}
-
-/usr/sbin/openvpn \
-    --daemon \
-    --config     ${DIR_C}/${SERVER_ID}.conf \
-    --writepid   ${TARGET_DIR}/logs/openvpn.pid \
-    --log-append ${TARGET_DIR}/logs/openvpn.log
-
-sleep 10
-
-/usr/bin/node ../updateStreams.js ${NUM_CHANNEL}
-/usr/bin/node ../updateEdges.js
-
-PID="$(cat ${TARGET_DIR}/logs/openvpn.pid)"
-
-kill -15 ${PID}
+/usr/sbin/openvpn                              \
+    --config         ${DIR_N}/ovpn_udp/${CONF} \
+    --auth-user-pass ${DIR_N}/auth.txt         \
+    --log-append     ${DIR_N}/logs/${TS}.log   \
+    --writepid       ${DIR_N}/logs/${TS}.pid   \
+    --daemon
