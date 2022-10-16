1fd3f71efd07565f6b82c9ebf435098bfe95fa98
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-10-16 16:59:44 +0800

    mod connect.sh, select country in argv

--- kukudy/scripts/connect.sh
+++ kukudy/scripts/connect.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-SERVER_ID="$1"
+if [ $# != 1 ] ; then
+    echo -e "
+SYNOPSIS
+    sudo bash connect.sh COUNTRY_CODE
+"
+    exit 0
+fi
+
+DIR_K="/home/nslab-m06/Samuel/1101-toy-project/kukudy"
+SERVER_ID="$(/usr/bin/node ${DIR_K}/utils/getServersRecommendations.js $1)"
+
+if [ $? == 1 ] ; then
+    exit 0
+fi
 
 DIR_N="/etc/openvpn/nordvpn"
 CONF="${SERVER_ID}.nordvpn.com.udp.ovpn"
