a5a5391259bf2c481c2fe8244c6e48ed8f5687af
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2022-10-19 12:15:07 +0800

    style waitForVPN

--- kukudy/utils/waitForVPN.js
+++ kukudy/utils/waitForVPN.js
@@ -2,10 +2,10 @@
 const axios = require('axios');
 const { sleep } = require('./utils');
 
-const url = 'https://nordvpn.com/wp-admin/admin-ajax.php';
-const config = { params: { action: 'get_user_info_data' } };
-
-const reqVPNStatus = async () => axios.get(url, config);
+const reqVPNStatus = () => axios.get(
+  'https://nordvpn.com/wp-admin/admin-ajax.php',
+  { params: { action: 'get_user_info_data' } },
+);
 
 const waitForVPN = async () => {
   let { data } = await reqVPNStatus();
@@ -14,11 +14,7 @@ const waitForVPN = async () => {
     await sleep(3000);
     data = (await reqVPNStatus()).data;
   }
-
-  return data;
 };
-
-module.exports = { reqVPNStatus };
 
 if (require.main === module) {
   waitForVPN();
