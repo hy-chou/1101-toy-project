f971297848305499c290e5db4a54c0cb02e2d985
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-03-15 12:46:51 +0800

    mod heatmapPvT, style

--- analyze/heatmapPvT.py
+++ analyze/heatmapPvT.py
@@ -1,52 +1,50 @@
 from collections import Counter
+from json import loads
 from os import getcwd, listdir
 from os.path import basename
 
 import matplotlib.pyplot as plt
 from numpy import arange, array
 
+timestamps = []
 file2counter = dict()
-prefixes = set()
+for file in sorted(listdir('./info')):
+    counter = Counter()
 
-files = sorted(listdir('./edgs'))
-for file in files:
-    c = Counter()
+    timestamps.append(file[:-4])
 
-    with open(f'./edgs/{file}') as f:
+    with open(f'./info/{file}') as f:
         lines = f.readlines()
     for line in lines:
         l = line[:-1].split('\t')
-        if (l[1].startswith('video-edge-')):
-            prefix = l[1][11:23]
-            c[prefix] += 1
+        if (l[2].startswith('{"NODE"')):
+            info = loads(l[2])
+            prefix = info["NODE"][11:]
+            counter[prefix] += 1
+    file2counter[file] = counter
 
-    file2counter[file] = c
-
-for file, c in file2counter.items():
-    for prefix, n in c.items():
-        prefixes.add(prefix)
-
-prefixes = sorted(list(prefixes), key=lambda x: x[-5:]+x[:6])
+prefix_set = set()
+for counter in file2counter.values():
+    for prefix in counter.keys():
+        prefix_set.add(prefix)
+prefix_set = sorted(list(prefix_set), key=lambda x: x[-5:]+x[:6])
 
 table = []
-for file, c in file2counter.items():
+for counter in file2counter.values():
     row = []
-    for pf in prefixes:
-        row.append(c[pf])
+    for pf in prefix_set:
+        row.append(counter[pf])
     table.append(row)
 
 
-fig, ax = plt.subplots(figsize=(len(files)/4, len(prefixes)/5))
+fig, ax = plt.subplots(figsize=(len(timestamps)/4, len(prefix_set)/5))
 
 table = array(table).T
 im = ax.imshow(table, vmin=0, cmap='Blues', interpolation='none')
 
-times = []
-for file in files:
-    times.append(file[:-4])
-ax.set_xticks(arange(len(times)), labels=times)
+ax.set_xticks(arange(len(timestamps)), labels=timestamps)
 plt.setp(ax.get_xticklabels(), rotation=90, ha="right", rotation_mode="anchor")
-ax.set_yticks(arange(len(prefixes)), labels=prefixes)
+ax.set_yticks(arange(len(prefix_set)), labels=prefix_set)
 ax.figure.colorbar(im, ax=ax)
 ax.spines[:].set_visible(False)
 
